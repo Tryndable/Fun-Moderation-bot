4ade26632ce85d90e1195ef460684fd44b183949
Tryndable <91785436+Tryndable@users.noreply.github.com>
2021-10-02 21:40:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ const bot = new Client({ disableMentions: 'everyone' });
 const fs = require("fs");
 const db = require('quick.db');
 //============================================================================================================================================================================================================
-const express = require('express')
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hey there!'))
-
-app.listen(port, () =>
-console.log(`Your app is listening a http://localhost:${port}`)
-);
 
 //====================================================================================COLLECTIONS REQUIRED ON READY===========================================================================================
 bot.commands = new Collection();
